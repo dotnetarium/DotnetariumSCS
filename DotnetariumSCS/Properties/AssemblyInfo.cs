5bdda9424636f621a27f7eb9e7a37fde2271edf0
Dmitry <dmodeus@gmail.com>
2024-06-29 20:54:51 -0400

    prep to build with dotnet build

--- DotnetariumSCS/Properties/AssemblyInfo.cs
+++ DotnetariumSCS/Properties/AssemblyInfo.cs
@@ -3,17 +3,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DotnetariumSCS")]
-[assembly: AssemblyDescription("Security static code analyzer for .NET based on Security  Code Scan")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/dotnetarium/DotnetariumSCS")]
-[assembly: AssemblyProduct("DotnetariumSCS")]
-[assembly: AssemblyCopyright("© Dotnetarium")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -29,8 +18,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: InternalsVisibleTo("DotnetariumSCS.Test")]
