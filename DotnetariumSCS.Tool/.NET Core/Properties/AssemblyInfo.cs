0cc493d9860ff1820ddb4434342ec9825054ec71
Dmitry <dmodeus@gmail.com>
2024-07-04 12:10:23 -0400

    bump version + attempt to resolve assembly compiler conflict (#10)
    * bump version + attempt to resolve assembly compiler conflict
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * Update dotnet-buildonly.yml
    
    * prep to build with dotnet build
    
    * move global.json
    
    * project level global file
    
    * build analyzers with dotnet
    
    * just build analyzers and upload
    
    * use .net 6 only for analyzers
    
    * 1.0.3
    
    * generate nuget with analyzers for vs 2019 and 2022
    
    * avoid rewriting the same file (aka locking)
    
    * restore the build
    
    * no need to have global.json
    
    * restoring deps for Roslyn 3.11.0
    
    * same version of codeanalysis.analyzers

--- DotnetariumSCS.Tool/.NET Core/Properties/AssemblyInfo.cs
+++ DotnetariumSCS.Tool/.NET Core/Properties/AssemblyInfo.cs
@@ -5,14 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dotnetarium SCS")]
-[assembly: AssemblyDescription("Security static code analyzer for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/dotnetarium/DotnetariumSCS")]
-[assembly: AssemblyProduct("Dotnetarium SCS")]
-[assembly: AssemblyCopyright("© Dotnetarium")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -28,6 +21,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
 [assembly: NeutralResourcesLanguage("en")]
